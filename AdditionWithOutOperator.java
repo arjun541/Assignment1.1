78b3d35e161c71dfe6cb6a26dcef13afe3a89ef9
arjun541 <bmkr.541@gmail.com>
2017-01-13 19:40:37 +0530

    Create AdditionWithOutOperator.java

---
+++ AdditionWithOutOperator.java
@@ -0,0 +1,19 @@
+public class AdditionWithOutOperator
+{
+
+
+
+public static void main(String args[])
+	{
+		int a=4,b=3;
+		while(b!=0)
+		{
+		  int c=(a&b);
+		  a=a^b;
+		  b=c<<1;
+		}
+
+		System.out.println(a);
+	}
+  
+  }
